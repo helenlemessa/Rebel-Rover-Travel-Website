a27efacf17fc465bb23570b92da7690c5892bc14
Abdurazakm <abdulkem5472@gmail.com>
2025-04-05 23:11:53 +0300

    Add placeholder pages for singleBlog

---
+++ src/pages/SingleBlog.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function SingleBlog() {
+  return (
+    <div>
+      <div className="p-4 text-xl">Welcome to the single Page</div>;
+    </div>
+  );
+}
+
+export default SingleBlog;
