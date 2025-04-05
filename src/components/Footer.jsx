f99316c91ca9c88eedcd563d3fcfd4437ef6865b
Abdurazakm <abdulkem5472@gmail.com>
2025-04-05 23:08:54 +0300

    Create Footer with site branding and © 2025 copyright

---
+++ src/components/Footer.jsx
@@ -0,0 +1,13 @@
+// src/components/Footer.jsx
+const Footer = () => {
+    return (
+      <footer className="bg-black text-white py-6 mt-10">
+        <div className="max-w-7xl mx-auto text-center text-sm">
+          © 2025 Rebel Rover. All rights reserved.
+        </div>
+      </footer>
+    );
+  };
+  
+  export default Footer;
+  
