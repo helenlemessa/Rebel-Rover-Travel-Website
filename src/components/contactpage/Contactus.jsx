a7ae4e1f08f754f29e19cf314a62e614fd74236c
Helen <helenzelalem1996@gmail.com>
2025-04-08 19:07:56 +0300

    conatctpage

--- src/components/contactpage/Contactus.jsx
+++ src/components/contactpage/Contactus.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
-import Contact from './components/Contact.jsx';
-import ContactForm from './components/ContactForm.jsx';  
+import Contact from './contactcomponents/Contact.jsx';
+import ContactForm from './contactcomponents/ContactForm.jsx';  
 import { Link } from "react-router-dom"; 
 export default function App() {
   return (
