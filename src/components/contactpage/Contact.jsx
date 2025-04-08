e771da9694e6f81ad0512e57a0100d9ca12ba87b
Helen <helenzelalem1996@gmail.com>
2025-04-08 19:07:21 +0300

    contactpage

---
+++ src/components/contactpage/Contact.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Contactus from './components/Contactus.jsx';
+import ContactForm from './components/ContactForm.jsx';  
+ 
+export default function Contact() {
+  return (
+    <>
+      <Contactus />
+      <ContactForm />
+      
+   </>
+  
+  )
+};
---
+++ src/components/contactpage/Contactus.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Contact from './components/Contact.jsx';
+import ContactForm from './components/ContactForm.jsx';  
+import { Link } from "react-router-dom"; 
+export default function App() {
+  return (
+    <>
+      <Contact />
+      <ContactForm />
+      
+   </>
+  
+  )
+};
