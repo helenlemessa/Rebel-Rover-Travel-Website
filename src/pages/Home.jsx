a9c0923128b2d08741b1f4d2c0883b85d7f96393
mechatemesgen <ililnaafbarihe94@gmail.com>
2025-04-09 10:24:35 +0300

    Home Page added

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,27 +1,15 @@
-import React from 'react'
-import { useState } from 'react'
-import Logo from '../assets/logo1 1.svg'
-import SearchModal from '../components/ui/SearchModal'
-import Hero from '../components/ui/Hero'
-
-import Horobanner from '../assets/PlaceholderImages/HeroBanner.png'
-
-function Home() {
+import React from 'react';
+import HeroSection from '../components/ui/HeroSection';
+import Destinations from '../components/ui/Destinations';
+import Services from '../components/ui/Services';
+import Testimonial from '../components/ui/Testimonial';
+export default function Home() {
   return (
-
-    <section className=''>
-      <div className="hero bg-cover bg-center text-white p-8 sm:p-12 lg:p-20 min-h-screen flex flex-col items-center justify-center"
-         style={{ backgroundImage: `url(${Horobanner})` }}>
-             <h1 className="text-5xl sm:text-6xl lg:text-6xl font-bold" style={{ textShadow: '4px 4.5px 2px rgba(0, 0, 0, 0.2)' }}>
-               About Us
-             </h1>
-             <p className="pt-4 text-xl sm:text-2xl lg:text-2.5xl font-bold" style={{ textShadow: '4px 4.5px 2px rgba(0, 0, 0, 0.2)' }}>
-               Home &gt; About Us
-             </p>
-           </div>
-
-   </section>
+   <>
+   <HeroSection />
+   <Destinations/>
+   <Services />
+   <Testimonial/>
+   </>
   )
 }
-
-export default Home
