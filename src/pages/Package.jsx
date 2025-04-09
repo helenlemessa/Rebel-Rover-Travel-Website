59582c54f2fc424f03bd5deaba0d6f8cd409a4ee
mechatemesgen <ililnaafbarihe94@gmail.com>
2025-04-09 09:08:45 +0300

    H-M added

--- src/pages/Package.jsx
+++ src/pages/Package.jsx
@@ -1,14 +1,22 @@
 import React from 'react'
 import Logo from '../assets/logo1 1.svg'
+import Travlebanner from '../assets/PlaceholderImages/TravlePackageBanner.png'
+
 
 function Package() {
   return (
-    <div className='flex flex-col items-center justify-center min-h-screen bg-gray-400'>
-       <div>
-          <img src={Logo} alt="logo" className='h-32' />
-       </div>
-        <h1 className='text-3xl font-bold text-center'>Packages Page</h1>
-      </div>
+     <section className=''>
+          <div className="hero bg-cover bg-center text-white p-8 sm:p-12 lg:p-20 min-h-screen flex flex-col items-center justify-center"
+             style={{ backgroundImage: `url(${Travlebanner})` }}>
+                 <h1 className="text-5xl sm:text-6xl lg:text-6xl font-bold" style={{ textShadow: '4px 4.5px 2px rgba(0, 0, 0, 0.2)' }}>
+                   Travel Packages
+                 </h1>
+                 <p className="pt-4 text-xl sm:text-2xl lg:text-2.5xl font-bold" style={{ textShadow: '4px 4.5px 2px rgba(0, 0, 0, 0.2)' }}>
+                   Home &gt; Package
+                 </p>
+               </div>
+    
+       </section>
   )
 }
 
